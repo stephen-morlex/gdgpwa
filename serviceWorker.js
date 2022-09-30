7bf60a3cc0ae5637588fb9cc2fd224a952d6045e
Stephen Victor <stephenvicson@gmail.com>
2022-09-30 12:14:48 +0200

    Resources refactored

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,5 +1,5 @@
 // assets to store in the cache.
-const staticDevCoffee = "gdg-devfeast-site-v1"
+const staticGDG = "gdg-devfeast-site-v1"
 const assets = [
   "/",
   "/index.html",
@@ -8,17 +8,23 @@ const assets = [
   "/images/1.webp",
   "/images/2.webp",
   "/images/3.webp",
+  "/images/4.webp",
  
 ]
 
 //  it returns a promise, which helps us store our assets in the cache with
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
+    caches.open(staticGDG).then(cache => {
       cache.addAll(assets)
     })
   )
 })
+
+
+self.addEventListener("activate", event => {
+  console.log("Service worker activated");
+});
 
 // let's fetch our cache.
 self.addEventListener("fetch", fetchEvent => {
